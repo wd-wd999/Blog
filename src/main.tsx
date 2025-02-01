e9624a4ad55342e55df0d4bfc664fe1971a6e846
daisukew <dais.watanabe@scsk.jp>
2025-02-01 15:28:14 +0900

    Second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-
+import Login from '../src/pages/Login.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
---
+++ src/pages/Login.tsx
@@ -0,0 +1,42 @@
+import { Authenticator, translations } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import '../App.css'
+import Page1 from '../App.tsx'
+import { I18n } from "aws-amplify/utils"
+import { Amplify } from "aws-amplify";
+import { ResourcesConfig } from "@aws-amplify/core";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+const userPoolClientId = import.meta.env.VITE_REACT_APP_USER_POOL_CLIENT_ID;
+const userPoolId = import.meta.env.VITE_REACT_APP_USER_POOL_ID;
+const awsConfig: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolClientId: userPoolClientId,
+      userPoolId: userPoolId,
+    },
+  },
+};
+Amplify.configure(awsConfig);
+
+function Login() {
+  return (
+    <BrowserRouter>
+      <Authenticator>
+        {({ user }) => (
+          <main>
+            {user && (
+              <Routes>
+                <Route path="/" element={<Page1 />} />
+              </Routes>
+            )}
+          </main>
+        )}
+      </Authenticator>
+    </BrowserRouter>
+  );
+}
+
+I18n.putVocabularies(translations)
+I18n.setLanguage("ja")
+
+export default Login;
