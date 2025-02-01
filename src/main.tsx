a4f65c7a901a0c17b5888f8e4566e26e3ac01183
daisukew <dais.watanabe@scsk.jp>
2025-02-01 18:34:09 +0900

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../src/pages/Login.tsx'
 import Costcheck from '../src/pages/Costcheck.tsx'
-import Game from '../src/pages/game.tsx'
+import Game from '../src/pages/Game.tsx'
 import Home from './Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
